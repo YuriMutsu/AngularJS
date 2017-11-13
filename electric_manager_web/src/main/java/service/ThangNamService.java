db3face48b095e415bdacf90b4d40534a3554498
TMTAI-LT\tranm <tranminhtai1304@gmail.com>
2017-11-13 20:11:21 +0700

    server cập nhật thêm : - getThang - getNam

--- electric_manager_web/src/main/java/service/ThangNamService.java
+++ electric_manager_web/src/main/java/service/ThangNamService.java
@@ -9,48 +9,49 @@ import models.Thang;
 import ninja.Result;
 import ninja.Results;
 import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static util.Constant.MA_NAM;
-import static util.Constant.TABLE_NAM;
+import static util.Constant.*;
 
 /**
  * Created by tranm on 08-Nov-17.
  */
 public class ThangNamService extends DatabaseUtility{
     public Result getNam(){
-        List<Nam> array = new ArrayList<Nam>();
+        JSONArray array = new JSONArray();
         MongoClient mongoClient = new MongoClient();
         MongoCollection collection = db.getCollection(TABLE_NAM);
         FindIterable<Document> iterable = collection.find();
         iterable.forEach(new Block<Document>() {
             @Override
             public void apply(Document document) {
-                Nam nam = new Nam();
-                nam.setManam(document.getString(MA_NAM));
-                array.add(nam);
+                JSONObject json = new JSONObject();
+                json.put(MA_NAM, document.getString(MA_NAM));
+                array.put(json);
             }
         });
         mongoClient.close();
-        return Results.json().render(array);
+        return Results.text().render(array);
     }
 
     public Result getThang(){
-        List<Thang> array = new ArrayList<Thang>();
+        JSONArray array = new JSONArray();
         MongoClient mongoClient = new MongoClient();
-        MongoCollection collection = db.getCollection(TABLE_NAM);
+        MongoCollection collection = db.getCollection(TABLE_THANG);
         FindIterable<Document> iterable = collection.find();
         iterable.forEach(new Block<Document>() {
             @Override
             public void apply(Document document) {
-                Thang thang = new Thang();
-                thang.setMathang(document.getString(MA_NAM));
-                array.add(thang);
+                JSONObject json = new JSONObject();
+                json.put(MA_THANG, document.getString(MA_THANG));
+                array.put(json);
             }
         });
         mongoClient.close();
-        return Results.json().render(array);
+        return Results.text().render(array);
     }
 }
